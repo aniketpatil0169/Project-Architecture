ca4a8b73d3d10c9c894fefa0329c17a252522131
aniketpatil0169 <148536939+aniketpatil0169@users.noreply.github.com>
2024-04-03 22:08:15 +0530

    Add files via upload

---
+++ BusinessAccessLayer/IproductBL.cs
@@ -0,0 +1,9 @@
+﻿using Models;
+
+namespace BusinessAccessLayer
+{
+    public interface IproductBL
+    {
+        bool CreateProduct(Product product);
+    }
+}
---
+++ BusinessAccessLayer/ProductBL.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class ProductBL : IproductBL
+    {
+        IProductDB _db;
+
+        public ProductBL(IProductDB db)
+        {
+            _db = db;
+        }
+
+        public bool CreateProduct(Product product)
+        {
+            return _db.CreateProduct(product);
+        }
+    }
+}
